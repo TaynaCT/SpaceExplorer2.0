0ac15c5f4e55de098ab0d2bca231bd2d89d02082
Vítor Meira <vhcm21@hotmail.com>
2017-04-10 21:07:33 +0100

    Câmara quase pronta, falta o vosso feedback mais o zoom, se quiserem

--- SpaceExplores_PROJECT/Assets/_Script/CameraController.cs
+++ SpaceExplores_PROJECT/Assets/_Script/CameraController.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    public float speed;
+
+    Vector3 cameraLookAt;
     Transform tileTransform;
     Vector3 position;
+    float angle;
 
 	// Use this for initialization
 	void Start () {
+        Cursor.lockState = CursorLockMode.Confined;
+        angle = 0f;
         tileTransform = GameObject.FindGameObjectWithTag("tile").transform;
+        cameraLookAt = tileTransform.position + Vector3.up * tileTransform.localScale.y / 2;
         position = new Vector3(tileTransform.position.x +
             tileTransform.localScale.x / 2, 26, 
             tileTransform.position.z + tileTransform.localScale.z / 2);
         transform.position = position;
-        transform.LookAt(tileTransform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float angle = mouseX * Time.deltaTime * speed;
+            transform.RotateAround(cameraLookAt, Vector3.up, angle);
+        }
+    }
 }
