fdee03d9c88bda8027cd14078bcbaa255d13cfd2
Vítor Meira <vhcm21@hotmail.com>
2017-04-10 17:07:26 +0100

    Definição da câmara
    Só falta apontar para o centro da superfície e adicionar-lhe uns
    controles xD

---
+++ SpaceExplores_PROJECT/Assets/_Script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Transform tileTransform;
+    Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        tileTransform = GameObject.FindGameObjectWithTag("tile").transform;
+        position = new Vector3(tileTransform.position.x +
+            tileTransform.localScale.x / 2, 26, 
+            tileTransform.position.z + tileTransform.localScale.z / 2);
+        transform.position = position;
+        transform.LookAt(tileTransform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
