79eb88875ac04029d93e1f95b9b6a183cad0a793
Tayná Tinôco <a10729@alunos.ipca.pt>
2017-04-30 16:14:30 +0100

    PoupText Aparece o texto, e depois é destruido ao fim da anima ção

---
+++ SpaceExplores_PROJECT/Assets/_Script/FloatingText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Classe q controla a o texto que aparecem na tela do jogo
+
+public class FloatingText : MonoBehaviour {
+
+    public Animator textAnimator;
+    float timer = 10;
+   
+    void Start()
+    {
+        textAnimator.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer -= .5f;
+
+        if (timer < 0)
+        {
+            textAnimator.SetBool("FadeIN", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            textAnimator.SetBool("FadeIN", false);
+            textAnimator.SetBool("FadeOUT", true);
+
+            AnimatorClipInfo[] clipInfo = textAnimator.GetCurrentAnimatorClipInfo(0);
+            Destroy(gameObject, clipInfo[0].clip.length);
+        }
+
+        
+
+        
+    }
+	 
+}
