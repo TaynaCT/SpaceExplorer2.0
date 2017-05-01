875295a9dc7c0ee0bb082a7bd9ef14fc41d57bbf
Tayná Tinôco <a10729@alunos.ipca.pt>
2017-05-01 13:22:49 +0100

    Iniciação de teste de automatização do texto a função Resources.Load() não esta a encontrar os prefabs nos ficheiros

--- SpaceExplores_PROJECT/Assets/_Script/FloatingText.cs
+++ SpaceExplores_PROJECT/Assets/_Script/FloatingText.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //Classe q controla a o texto que aparecem na tela do jogo
 
 public class FloatingText : MonoBehaviour {
 
     public Animator textAnimator;
-    float timer = 10;
+    float timer = 20; //timer para aparição do texto após iniciado a aplicação
+    private Text screenText; //texto que deverá aparecer na tela
    
     void Start()
     {
         textAnimator.GetComponent<Animator>();
+        screenText = textAnimator.GetComponent<Text>();
     }
 
     void Update()
@@ -28,13 +31,20 @@ public class FloatingText : MonoBehaviour {
             textAnimator.SetBool("FadeIN", false);
             textAnimator.SetBool("FadeOUT", true);
 
+            //ao fim da animação, o objeto referente ao texto é destruido
             AnimatorClipInfo[] clipInfo = textAnimator.GetCurrentAnimatorClipInfo(0);
-            Destroy(gameObject, clipInfo[0].clip.length);
+            Destroy(gameObject, clipInfo[0].clip.length);            
         }
-
-        
-
-        
+                
+    }
+    
+    /// <summary>
+    /// função que recebe que passa o texto a ser mostrado na tela
+    /// </summary>
+    /// <param name="text"></param>
+    public void SetText(string text)
+    {
+        screenText.text = text;
     }
 	 
 }
---
+++ SpaceExplores_PROJECT/Assets/_Script/FloatingTextController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextController : MonoBehaviour {
+
+    private static FloatingText screenText;
+    private static GameObject canvas;
+
+    string[] textArray = { "Ola", "como vai vc?", "vlw" };
+
+    public static void Initialize()
+    {
+        canvas = GameObject.Find("Canvas");
+        screenText = Resources.Load<FloatingText>("_Prefabs/TextParent.prefab");       
+    }
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            CreatingText(textArray[0]);
+        }
+    }
+
+    public static void CreatingText(string text)
+    {
+        FloatingText instane = Instantiate(screenText);
+        instane.transform.SetParent(canvas.transform, false);
+
+        instane.SetText(text);
+    }
+
+}
