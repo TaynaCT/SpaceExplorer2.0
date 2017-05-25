69e18510f2e1207e588d61472b8d0f0d7cce1065
Vítor Meira <vhcm21@hotmail.com>
2017-05-25 12:45:39 +0100

    Outro update

---
+++ SpaceExplores_PROJECT/Assets/_Script/Soundtrack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour {
+
+    private AudioSource sound;
+    public AudioClip[] backgroundMusic;
+    int sortedCount;
+
+	// Use this for initialization
+	void Start () {
+        sound = GetComponent<AudioSource>();
+        sortedCount = 9;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < sortedCount; i++)
+        {
+            int selection = Random.Range(0, sortedCount);
+            sortedCount--;
+        }
+
+	}
+}
